fcc33aec9c484d8b696218df7459c9bc1722cee2
Ajla115 <korman.ajla115@gmail.com>
2025-01-14 17:04:20 +0100

    hooks

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,5 +1,9 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { getAllCategories, createCategory, suggestGenre } from "../api/genreApi";
+import {
+  getAllCategories,
+  createCategory,
+  suggestGenre,
+} from "../api/genreApi";
 import { toast } from "react-toastify";
 
 export const useGetAllCategories = () => {
@@ -13,7 +17,6 @@ export const useGetAllCategories = () => {
 
   return query;
 };
-
 
 export const useCreateCategory = () => {
   const queryClient = useQueryClient();
@@ -30,23 +33,23 @@ export const useCreateCategory = () => {
   });
 };
 
-
 export const useSuggestGenre = () => {
-    return useMutation({
-      mutationFn: suggestGenre,
-      onSuccess: (data: string) => {
-        if (data.startsWith("Error:")) {
-          toast.error(data);
-        } else {
-          toast.success(`AI recommended this genre: ${data}`, {
-            autoClose: 5000,
-          });
-        }
-      },
-      onError: (error: any) => {
-        toast.error(
-          error.response?.data?.message || "Something went wrong, please try again later."
-        );
-      },
-    });
-  };
+  return useMutation({
+    mutationFn: suggestGenre,
+    onSuccess: (data: string) => {
+      if (data.startsWith("Error:")) {
+        toast.error(data);
+      } else {
+        toast.success(`AI recommended this genre: ${data}`, {
+          autoClose: 5000,
+        });
+      }
+    },
+    onError: (error: any) => {
+      toast.error(
+        error.response?.data?.message ||
+          "Something went wrong, please try again later."
+      );
+    },
+  });
+};
